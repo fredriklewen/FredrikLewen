90440c1fa008f82dc7bf294195386bee2f120d51
Fredrik Lewén <124886941+fredriklewen@users.noreply.github.com>
2023-02-10 15:14:55 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -8,8 +8,10 @@ if (!online){
 addEventListener('DOMContentLoaded', function() {
 
     var url = window.location.pathname;
-    var filename = url.substring(url.lastIndexOf('/')+1);    
-    if (filename == "index.html") {
+    // var filename = url.substring(url.lastIndexOf('/')+1);    
+    // if (filename == "index.html") {
+    if (!url.includes("projects") && !url.includes("resume")) {
+
         // Scroll down
         document.getElementById("scroll-down").addEventListener("click", function() {
             window.scroll({
@@ -17,7 +19,9 @@ addEventListener('DOMContentLoaded', function() {
                 behavior: "smooth"
             });
         });
-    } else if (filename == "projects.html") {
+    // } else if (filename == "projects.html") {
+    } else if (url.includes("projects")) {
+
         document.getElementById("projects-back").addEventListener("click", function() {    
             const backButton = document.getElementById("projects-back");
             const urlParams = new URLSearchParams(window.location.search);
