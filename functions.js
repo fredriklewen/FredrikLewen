2e5e9a3032014b9fba3c6f5c2b58e7e930d3ec27
Fredrik Lewén <124886941+fredriklewen@users.noreply.github.com>
2023-02-11 20:28:04 +0100

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,49 +1,40 @@
-
 var online = window.navigator.onLine;
-if (!online){
+if (!online) {
     alert('You are not currently connected to the internet. Please try again later.');
     $("body").remove();
 }
 
-addEventListener('DOMContentLoaded', function() {
-
+addEventListener('DOMContentLoaded', function () {
     var url = window.location.pathname;
-    // var filename = url.substring(url.lastIndexOf('/')+1);    
-    // if (filename == "index.html") {
     if (!url.includes("projects") && !url.includes("resume")) {
-
         // Scroll down
-        document.getElementById("scroll-down").addEventListener("click", function() {
+        document.getElementById("scroll-down").addEventListener("click", function () {
             window.scroll({
                 top: window.innerHeight,
                 behavior: "smooth"
             });
         });
-    // } else if (filename == "projects.html") {
     } else if (url.includes("projects")) {
-
-        document.getElementById("projects-back").addEventListener("click", function() {    
+        document.getElementById("projects-back").addEventListener("click", function () {
             const backButton = document.getElementById("projects-back");
             const urlParams = new URLSearchParams(window.location.search);
             if (urlParams.get("origin") === "resume") {
-                backButton.href = "resume.html";
+                backButton.href = "../resume/";
             } else {
-                backButton.href = "index.html";
+                backButton.href = "../";
             }
         });
     }
 
     // Scroll to top
     var element = document.getElementsByClassName("scroll-up");
-    for(var i = 0; i < element.length; i++) {
-        element[i].addEventListener('click', function() {    
+    for (var i = 0; i < element.length; i++) {
+        element[i].addEventListener('click', function () {
             window.scroll({
                 top: 0,
                 behavior: "smooth"
             });
         });
     }
-
-    
 });
 
